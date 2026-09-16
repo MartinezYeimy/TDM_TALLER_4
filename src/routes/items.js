c556824aec3c7c3f42cd98813ca6e17b90fec7e9
MartinezYeimy <yeimartinez@uniboyaca.edu.co>
2026-09-16 09:01:14 -0500

    Migración proyecto

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,15 +1,9 @@
-const fs = require("fs");
-const path = require("path");
+import { Router } from "express";
+import { getAllItems, findItem, insertItem, modifyItem, removeItem } from "../db/db.js";
 
-const DATA_PATH = path.join(__dirname, "..", "data", "items.json");
+const router = Router();
 
-function readData() {
-    return JSON.parse(fs.readFileSync(DATA_PATH, "utf8"));
-}
 
-function writeData(data) {
-    fs.writeFileSync(DATA_PATH, JSON.stringify(data, null, 2));
-}
 
 function handleItemsRoutes(req, res) {
     if (!req.url.startsWith("/api/items")) return false;
